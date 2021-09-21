28b9b0ec82c0c9c66efe5098025c70aaebdd2b98
asm0dey <pavel.finkelshtein@gmail.com>
2021-09-21 11:16:20 +0300

    Adds unique badge colors

--- src/components/ConferencesSearchContainer.js
+++ src/components/ConferencesSearchContainer.js
@@ -1,57 +1,100 @@
 import React, { Component } from "react";
 import * as JsSearch from "js-search";
 import conferences from "../../static/conferences.yaml";
-import { Input, Form, Tag, Table, Layout, Row, Col } from 'antd';
-import 'antd/dist/antd.css';
-
+import { Input, Form, Tag, Table, Layout, Row, Col } from "antd";
+import "antd/dist/antd.css";
 
 const { Header, Content } = Layout;
+const randomColor = (string) => {
+  var sanitized = string.replace(/[^A-Za-z]/, "");
+  var letters = sanitized.split("");
 
+  //Determine the hue
+  var hue = Math.floor(
+    ((letters[0].toLowerCase().charCodeAt() - 96) / 26) * 360
+  );
+  var ord = "";
+  for (var i in letters) {
+    ord = letters[i].charCodeAt();
+    if ((ord >= 65 && ord <= 90) || (ord >= 97 && ord <= 122)) {
+      hue += ord - 64;
+    }
+  }
+
+  hue = hue % 360;
+
+  //Determine the saturation
+  var vowels = ["A", "a", "E", "e", "I", "i", "O", "o", "U", "u"];
+  var count_cons = 0;
+
+  //Count the consonants
+  for (i in letters) {
+    if (vowels.indexOf(letters[i]) === -1) {
+      count_cons++;
+    }
+  }
+
+  //Determine what percentage of the string is consonants and weight to 95% being fully saturated.
+  var saturation = (count_cons / letters.length / 0.95) * 100;
+  if (saturation > 100) saturation = 100;
+
+  //Determine the luminosity
+  var ascenders = ["t", "d", "b", "l", "f", "h", "k"];
+  var descenders = ["q", "y", "p", "g", "j"];
+  var luminosity = 50;
+  var increment = (1 / letters.length) * 50;
+
+  for (i in letters) {
+    if (ascenders.indexOf(letters[i]) !== -1) {
+      luminosity += increment;
+    } else if (descenders.indexOf(letters[i]) !== -1) {
+      luminosity -= increment * 2;
+    }
+  }
+  if (luminosity > 100) luminosity = 100;
+  return `hsl(${hue},${Math.floor(saturation)}%,${luminosity}%)`;
+};
 const columns = [
   {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-    render: (text, record)=>(
-      <a href={record.url}>{text}</a>
-    )
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+    render: (text, record) => <a href={record.url}>{text}</a>,
   },
   {
-    title: 'Start date',
-    dataIndex: 'date_start',
-    key: 'date_start',
+    title: "Start date",
+    dataIndex: "date_start",
+    key: "date_start",
   },
   {
-    title: 'End date',
-    dataIndex: 'date_end',
-    key: 'date_end',
+    title: "End date",
+    dataIndex: "date_end",
+    key: "date_end",
   },
   {
-    title: 'Price',
-    dataIndex: 'price',
-    key: 'price',
-    render: (text, record)=>(
-      <span>{`${record.currency} ${text}`}</span>
-    )
- },
-  {
-    title: 'Format',
-    dataIndex: 'format',
-    key: 'format',
+    title: "Price",
+    dataIndex: "price",
+    key: "price",
+    render: (text, record) => <span>{`${record.currency} ${text}`}</span>,
   },
   {
-    title: 'CFP deadline',
-    dataIndex: 'cfp_deadline',
-    key: 'cfp_deadline',
+    title: "Format",
+    dataIndex: "format",
+    key: "format",
   },
   {
-    title: 'Topics',
-    dataIndex: 'topics',
-    key: 'topics',
-    render: tags => (
+    title: "CFP deadline",
+    dataIndex: "cfp_deadline",
+    key: "cfp_deadline",
+  },
+  {
+    title: "Topics",
+    dataIndex: "topics",
+    key: "topics",
+    render: (tags) => (
       <>
-        {tags.map(tag => {
-          let color = tag.length > 5 ? 'geekblue' : 'green';
+        {tags.map((tag) => {
+          let color = colors[tag];
           return (
             <Tag color={color} key={tag}>
               {tag.toUpperCase()}
@@ -59,8 +102,17 @@ const columns = [
           );
         })}
       </>
-    ),},
-]
+    ),
+  },
+];
+
+const colors = Object.fromEntries(
+  new Map(
+    [...new Set(conferences.conferences.flatMap((item) => item.topics))].map(
+      (item) => [item, randomColor(item)]
+    )
+  )
+);
 
 class Search extends Component {
   state = {
@@ -118,53 +170,25 @@ class Search extends Component {
         <Header>
           <h1>Hi!</h1>
         </Header>
-        <Content style={{padding: '0 50px'}}>
+        <Content style={{ padding: "0 50px" }}>
           <Row>
             <Col span={24}>
-            <Form onSubmit={this.handleSubmit}>
-              <Input
-                id="Search:"
-                value={searchQuery}
-                onChange={this.searchData}
-                placeholder="Search"
-                style={{ margin: "0 auto", width: "400px" }}
-              />
-          </Form>
+              <Form onSubmit={this.handleSubmit}>
+                <Input
+                  id="Search:"
+                  value={searchQuery}
+                  onChange={this.searchData}
+                  placeholder="Search"
+                  style={{ margin: "0 auto", width: "400px" }}
+                />
+              </Form>
             </Col>
-            </Row>
-            {/* <table>
-              <thead>
-                <tr>
-                  <th>Conference</th>
-                  <th>Topic</th>
-                  <th>Price</th>
-                  <th>Dates</th>
-                  <th>Format</th>
-                </tr>
-              </thead>
-              <tbody>
-                {queryResults.map((item) => {
-                  return (
-                    <tr key={`row_${item.url}`}>
-                      <td>
-                        <a href={item.url}>{item.name}</a>
-                      </td>
-                      <td>{item.topics.join(", ")}</td>
-                      <td>
-                        {item.currency} {item.price}
-                      </td>
-                      <td>
-                        {item.date_start} — {item.date_end}
-                      </td>
-                      <td>{item.format}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table> */}
-            <Row>
-              <Col span={24}><Table columns={columns} dataSource={queryResults}/></Col>
-            </Row>
+          </Row>
+          <Row>
+            <Col span={24}>
+              <Table columns={columns} dataSource={queryResults} />
+            </Col>
+          </Row>
         </Content>
       </Layout>
     );
