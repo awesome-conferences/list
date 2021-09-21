0845fbcbc5344af7a0193435748ab3f2d9700353
asm0dey <pavel.finkelshtein@gmail.com>
2021-09-21 09:12:30 +0300

    Reformatting

--- src/components/ConferencesSearchContainer.js
+++ src/components/ConferencesSearchContainer.js
@@ -1,63 +1,65 @@
-import React, { Component } from "react"
-import * as JsSearch from "js-search"
-import conferences from "../../static/conferences.yaml"
+import React, { Component } from "react";
+import * as JsSearch from "js-search";
+import { conferences } from "../../static/conferences.yaml";
 class Search extends Component {
   state = {
-    confList: conferences.conferences,
+    confList: conferences,
     search: [],
     searchResults: [],
     isLoading: true,
     isError: false,
     searchQuery: "",
-  }
+  };
 
-  async componentDidMount(){
-    this.rebuildIndex()
+  async componentDidMount() {
+    this.rebuildIndex();
   }
 
   /**
    * rebuilds the overall index based on the options
    */
   rebuildIndex = () => {
-    const { confList} = this.state
-    const dataToSearch = new JsSearch.Search("url")
-    dataToSearch.indexStrategy = new JsSearch.PrefixIndexStrategy()
-    dataToSearch.sanitizer = new JsSearch.LowerCaseSanitizer()
+    const { confList } = this.state;
+    const dataToSearch = new JsSearch.Search("url");
+    dataToSearch.indexStrategy = new JsSearch.PrefixIndexStrategy();
+    dataToSearch.sanitizer = new JsSearch.LowerCaseSanitizer();
 
-    dataToSearch.searchIndex = new JsSearch.TfIdfSearchIndex("url")
+    dataToSearch.searchIndex = new JsSearch.TfIdfSearchIndex("url");
 
-// searching by these fields only:
-    dataToSearch.addIndex("name")
-    dataToSearch.addIndex("date_start")
-    dataToSearch.addIndex("date_end")
-    dataToSearch.addIndex("topics")
+    // searching by these fields only:
+    dataToSearch.addIndex("name");
+    dataToSearch.addIndex("date_start");
+    dataToSearch.addIndex("date_end");
+    dataToSearch.addIndex("topics");
 
-    dataToSearch.addDocuments(confList) // adds the data to be searched
-    this.setState({ search: dataToSearch, isLoading: false })
-  }
+    dataToSearch.addDocuments(confList); // adds the data to be searched
+    this.setState({ search: dataToSearch, isLoading: false });
+  };
 
   /**
    * handles the input change and perform a search with js-search
    * in which the results will be added to the state
    */
-  searchData = e => {
-    const { search } = this.state
-    const queryResult = search.search(e.target.value)
-    this.setState({ searchQuery: e.target.value, searchResults: queryResult })
-  }
-  handleSubmit = e => {
-    e.preventDefault()
-  }
+  searchData = (e) => {
+    const { search } = this.state;
+    const queryResult = search.search(e.target.value);
+    this.setState({ searchQuery: e.target.value, searchResults: queryResult });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   render() {
-    const { confList, searchResults, searchQuery } = this.state
-    const queryResults = searchQuery === "" ? confList: searchResults
+    const { confList, searchResults, searchQuery } = this.state;
+    const queryResults = searchQuery === "" ? confList : searchResults;
     return (
       <div>
         <div>
           <form onSubmit={this.handleSubmit}>
             <div>
-              <label htmlFor="Search" style={{ paddingRight: "10px" }}>Search:</label>
+              <label htmlFor="Search" style={{ paddingRight: "10px" }}>
+                Search:
+              </label>
               <input
                 id="Search"
                 value={searchQuery}
@@ -79,27 +81,29 @@ class Search extends Component {
                 </tr>
               </thead>
               <tbody>
-                {queryResults.map(item => {
+                {queryResults.map((item) => {
                   return (
                     <tr key={`row_${item.url}`}>
-                      <td><a href={item.url}>{item.name}</a></td>
                       <td>
-                        {item.topics}
+                        <a href={item.url}>{item.name}</a>
                       </td>
+                      <td>{item.topics}</td>
                       <td>
                         {item.currency} {item.price}
                       </td>
-                      <td>{item.date_start} — {item.date_end}</td>
+                      <td>
+                        {item.date_start} — {item.date_end}
+                      </td>
                       <td>{item.format}</td>
                     </tr>
-                  )
+                  );
                 })}
               </tbody>
             </table>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
-export default Search
+export default Search;
