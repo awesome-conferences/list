7f88fe7c0e834926f964d19d23dca7f573ef60d5
Vitaly Sharovatov <vitaly.sharovatov@gmail.com>
2021-12-26 17:47:40 +0300

    fixed the url

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://awesome-conferences.github.io/list/",
     title: "Conferences",
   },
   plugins: [
